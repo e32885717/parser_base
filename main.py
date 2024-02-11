d0beffd648951521186bc0f8893ff7f84f551f71
3wifiparser <159683562+3wifiparser@users.noreply.github.com>
2024-02-11 20:24:49 +0500

    GZIP support, fix auth bug

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import db
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Annotated
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import random
 import string
+import zlib
+from typing import Callable
+from fastapi.routing import APIRoute
 
 app = FastAPI()
+
+
+class GzipRequest(Request):
+    async def body(self) -> bytes:
+        if not hasattr(self, "_body"):
+            body = await super().body()
+            if "gzip" in self.headers.getlist("Content-Encoding"):
+                body = zlib.decompress(body)
+            self._body = body
+        return self._body
+
+
+class GzipRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            request = GzipRequest(request.scope, request.receive)
+            return await original_route_handler(request)
+
+        return custom_route_handler
+
+app.router.route_class = GzipRoute
+
 security = HTTPBasic()
 tokens = []
 
--- salt.py
+++ salt.py
@@ -3,4 +3,4 @@ from base64 import b32encode, b64encode
 
 def generate_user_salt(username):
     username = b32encode(username.encode())
-    return b64encode(hashlib.sha512(username).digest())[0:20]
+    return (b64encode(hashlib.sha512(username).digest())[0:20]).decode()
